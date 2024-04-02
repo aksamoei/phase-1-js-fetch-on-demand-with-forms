b5fc5fd4cb98ad6fbc7920e0aa1172bd6a61297f
Abraham <abukip344@gmail.com>
2024-04-02 09:22:33 -0400

    fetch on demand

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const init = () => {
-  
+  const inputForm = document.querySelector("form");
+  inputForm.addEventListener("submit", function (event){
+    event.preventDefault();
+    const inputList = event.target.children;
+    const chosenId = inputList[1].value;
+
+    fetch(`http://localhost:3000/movies/${chosenId}`)
+    .then(response => response.json())
+    .then(function(movieData){
+        const movieDetails = document.getElementById("movieDetails");
+        const movieHeader = movieDetails.querySelector("h4");
+        const movieSummary = movieDetails.querySelector("p");
+        movieHeader.innerText = movieData.title;
+        movieSummary.innerText = movieData.summary;
+    })
+  });
 }
 
 document.addEventListener('DOMContentLoaded', init);
